4c20a2d6c79ee906770b1830891872ef421b9fbe
deipanema <miniharu.zip@gmail.com>
2023-06-10 09:45:05 +0900

    [기능 추가] 헤더 검색창 컴포넌트 추가
    1. tailwind.config.js 파일에 피그마에 정리되어 있는 커스텀 색상을 등록했습니다.
    2. package.json파일 안에 tailwindCSS 관련 라이브러리 dependencies에서 devDependencies로 옮겼습니다.
    3. Header 컴포넌트 안의 검색창은 재활용하기 위해서 Search 컴포넌트는 만들어 import 했습니다.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{js, jsx, ts, tsx}'],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'main-green': '#136743',
+        'sub-green': '#E8F3F5',
+        'dusty-green': '#E7ECEB',
+        'strong-black': '#171717',
+        'dusty-black': '#484848',
+        'dusty2-black': '#686868',
+        'modal-black': '#757575',
+        'normal-gray': '#BCBCBC',
+        'dusty-gray': '#DEDEDE',
+        'light-gray': '#ECECEC',
+        'bright-gray': '#F7F7F7',
+        'dusty-white': '#FDFDFD',
+      },
+    },
   },
   plugins: [],
 };
