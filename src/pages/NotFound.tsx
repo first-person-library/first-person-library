9c5c281d32f6b118300d434b4f9ef7539368f243
deipanema <miniharu.zip@gmail.com>
2023-07-22 14:04:11 +0900

    [기능 수정] 사용자가 요청한 웹 페이지가 존재하지 않을 때 나타나는 오류 페이지
    사용자에게 요청한 페이지가 존재하지 않는다는 사실을 알리는 역할을 합니다.

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -7,16 +7,11 @@ export default function NotFound() {
       <Icon
         src="notfound.png"
         alt="페이지를 찾을 수 없습니다."
-        className="w-52 md:w-64 lg:w-72 my-7 md:my-10 lg:my-12"
+        className="h-40 md:h-64 lg:h-72 my-7 md:my-10 lg:my-12"
       />
       <h2 className="text-xl md:text-3xl lg:text-4xl">
         페이지를 찾을 수 없습니다.
       </h2>
-      <p className="hidden md:block text-dusty2-black dark:text-normal-gray my-3 lg:my-5">
-        찾으시는 페이지가 존재하지 않거나, 사용할 수 없는 페이지입니다.
-        <br /> 입력하신 페이지의 주소가 정확한지 다시 한번 확인해 주시길
-        바랍니다.
-      </p>
     </div>
   );
 }
