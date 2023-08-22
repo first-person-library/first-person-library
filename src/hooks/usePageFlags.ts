fc0120c817fe5931e5ec03caba4daa076af71d6d
deipanema <miniharu.zip@gmail.com>
2023-08-22 18:24:27 +0900

    [리팩토링] usePageFlags 함수 코드 개선
    기존 코드에서 매개변수 처리 방식을 변경하고 불필요한 분기를 제거하였습니다.
    
    변경 전 코드:
    - 문자열 또는 문자열 배열을 인자로 받는 checkPagePath 함수를 통해 페이지 경로를 확인
    
    변경 후 코드:
    - rest parameter 를 사용하여 여러 개의 페이지 경로를 처리

--- src/hooks/usePageFlags.ts
+++ src/hooks/usePageFlags.ts
@@ -4,18 +4,14 @@ export default function usePageFlags() {
   const location = useLocation();
   const { pathname } = location;
 
-  const checkPagePath = (pagePaths: string | string[]): boolean => {
-    if (Array.isArray(pagePaths)) {
-      return pagePaths.some((path) => pathname.includes(path));
-    } else {
-      return pathname.includes(pagePaths);
-    }
+  const checkPagePath = (...pagePaths: string[]): boolean => {
+    return pagePaths.some((path) => pathname.includes(path));
   };
 
   const IS_HOME_PAGE = pathname === '/';
   const IS_MYCOMMENTS_PAGE = checkPagePath('/my');
   const IS_UPDATE_PAGE = checkPagePath('/my/');
-  const IS_WRITE_PAGE = checkPagePath(['/my/', '/new']);
+  const IS_WRITE_PAGE = checkPagePath('/my/', '/new');
   const IS_COMMENTS_PAGE = checkPagePath('/comments');
   const HAS_COMMENTS = IS_HOME_PAGE || IS_MYCOMMENTS_PAGE || IS_COMMENTS_PAGE;
 
